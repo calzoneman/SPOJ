a22dc2707a946d6fcfbf89b0e6bf44b4d75ac784
Calvin Montgomery <calzoneman@gmail.com>
2013-01-14 21:54:12 -0600

    Add comment to explain what z(n) does in fctrl.py

--- fctrl.py
+++ fctrl.py
@@ -2,6 +2,7 @@
 
 import math
 
+# Z(N) calculates the number of trailing 0s of N!
 def z(n):
     power = math.floor(math.log(n, 5))
     return sum([int(n / (5 ** k)) for k in range(1, power + 1)])
